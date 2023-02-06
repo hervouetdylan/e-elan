ed297411b6f524c14193805a0338f960c181af87
alvinlucas <alvin44.lucas@gmail.com>
2023-02-06 15:55:56 +0100

    home

--- e-elan/src/index.js
+++ e-elan/src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Footer from './components/footer/footer';
+import Home from './components/home/home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Home />
     <Footer />
   </React.StrictMode>
 );
