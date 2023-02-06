0bb3bb587186c5044eab7ff3ebcc4870e129e8d8
hervouetdylan <dylan.hervouet@ynov.com>
2023-02-06 16:00:48 +0100

    navbar

---
+++ e-elan/src/header/header.js
@@ -0,0 +1,52 @@
+import './header.css';
+import logo from './panier.png';
+
+const Logo = require('./panier.png');
+
+const Header = () => {
+  return (
+    <body>
+      <div class="header">
+        <ul id="menu-deroulant">
+	        <li><a href="#">Categories</a>
+		        <ul>
+              <li><a href="#">lien sous menu 1</a></li>
+              <li><a href="#">lien sous menu 1</a></li>
+		        </ul>
+	        </li>
+          <li><a href="#">Best-Sellers</a>
+		        <ul>
+              <li><a href="#">Lien sous menu 2</a></li>
+              <li><a href="#">Lien sous menu 2</a></li>
+		        </ul>
+	        </li>
+          <li><a href="#">Help</a>
+            <ul>
+              <li><a href="#">Lien sous menu 3</a></li>
+              <li><a href="#">Lien sous menu 3</a></li>
+            </ul>
+          </li>
+          <li><a href="#">About Us</a>
+            <ul>
+              <li><a href="#">Lien sous menu 3</a></li>
+              <li><a href="#">Lien sous menu 3</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Contact</a>
+            <ul>
+              <li><a href="#">Lien sous menu 3</a></li>
+              <li><a href="#">Lien sous menu 3</a></li>
+            </ul>
+          </li>
+        </ul>
+        <h1>E-Elan</h1>
+        <img src={Logo}></img>
+        <forms class="Sign Up">
+          <button type="text">Sign Up</button>
+        </forms>
+      </div>
+    </body>
+  );
+}
+
+export default Header;
