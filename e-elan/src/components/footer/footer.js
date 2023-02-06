99e1ebf49fda0a5444c8e91f22d1776b616a4fab
belenus <bellaud.matias@gmail.com>
2023-02-06 15:37:35 +0100

    footer finish

--- e-elan/src/components/footer/footer.js
+++ e-elan/src/components/footer/footer.js
@@ -3,22 +3,49 @@ import './footer.css';
 
 const Footer = () => (
   <div class="footer">
-  <footer style={{ position: 'fixed',marginLeft:'10%', bottom: 0, width: '100%' }}>
-    <div>
-    <h2>E-ELAN COMMERCE</h2>
-    <p>&copy; 2023 </p>
-    <p> Company Name</p>
-    </div>
-    <div>
-    <table>
-      <tr >
-        Catégories
-        <td>
-        </td>
-      </tr>
-    </table>
-    </div>
-  </footer>
+    <footer>
+        <div class="row">
+            <div class="colonne">
+                <h2>E-ELAN COMMERCE</h2>
+                <p>&copy; 2023 </p>
+                <p> Company Name</p>   
+            </div>
+            <div class="colonne">
+                <h3>Categories</h3>
+                <ul>
+                    <li><a href="#">High tech</a></li>
+                    <li><a href="#">Books</a></li>
+                    <li><a href="#">Furnitures</a></li>
+                    <li><a href="#">Cosmetics</a></li>
+                    <li><a href="#">Sports</a></li>
+                </ul>
+            </div>
+            <div class="colonne">
+                <h3>Help</h3>
+                <ul>
+                    <li><a href="#">FAQ</a></li>
+                    <li><a href="#">Report a probleme</a></li>
+                    <li><a href="#">Support</a></li>
+                </ul>
+            </div>
+            <div class="colonne">
+                <h3>About us</h3>
+                <ul>
+                    <li><a href="#">Our missions</a></li>
+                    <li><a href="#">Our values</a></li>
+                    <li><a href="#">Legal notice</a></li>
+                </ul>
+            </div>
+            <div class="colonne">
+                <h3>Contact</h3>
+                <ul>
+                    <li><a href="#">Email</a></li>
+                    <li><a href="#">Our socials</a></li>
+                    <li><a href="#">Contact us</a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
   </div>
 );
 
