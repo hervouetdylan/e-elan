e0e7b8275f19f7a3b9a6da15a44365fb79889570
belenus <bellaud.matias@gmail.com>
2023-02-07 00:33:38 +0100

    add button on menu

--- e-elan/src/components/menu/menu.js
+++ e-elan/src/components/menu/menu.js
@@ -2,15 +2,17 @@ import React from 'react';
 import './menu.css';
 import produit from './../../assets/produit.png'
 
-const testProduct = ['chips', 'water', 'ordi','etiquette','plot','terreau', 'wagon','mayonnaise']
+const testProduct =[['chips',5], ['water',2], ['ordi',500],['etiquette',3],['plot',6],['terreau',90]];
 let nombreOfItem = 0
 
 const Menu = () => (
+    
     <div class="allProducts">
         {testProduct.map((product) => (
             <div class = "objectCart ">
                 <img class="imageProduct"src={produit} alt="Produit" />
-                <p class="productName">{product}</p>
+                <p class="productName">{product[0]}</p>
+                <p class="productName">{product[1]}$</p>
                 <button class="boutonCart">add to cart</button>
             </div>
         ))}
