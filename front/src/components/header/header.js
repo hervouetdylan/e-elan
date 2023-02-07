22b175fe00301576b95f9b8f36c224a87db5635a
L0uxe <loulourxl@hotmail.com>
2023-02-07 21:32:47 +0100

    modified header

--- front/src/components/header/header.js
+++ front/src/components/header/header.js
@@ -10,21 +10,19 @@ export const Header = () => {
   return (
     <body>
       <div class="header">
-        <ul id="menu-deroulant">
-	        <li>
-            <Link to="/home"><a>Home</a></Link>
-	        </li>
-          <li>
-          <Link to="/menu"><a>Product</a></Link>
-	        </li>
-        </ul>
+        <div>
+            <Link to="/home"><button type="text">Home</button></Link>
+            <Link to="/menu"><button type="text">Product</button></Link>
+          </div>
         <Link to="/home"><img class="logoFooter" src={image} alt="image"/></Link>
+        <div>
           <forms class="SignUp">
             <Link to="/signUp"><button type="text">Sign up</button></Link>
           </forms>
           <forms class="LogIn">
             <Link to="/loginPage"><button type="text">Log in</button></Link>
           </forms>
+          </div>
       </div>
     </body>
   );
